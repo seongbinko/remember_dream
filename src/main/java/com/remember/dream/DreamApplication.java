aedcddb80e3c90a18d8b24a4e39c06565df47720
seongbinko <wnrhd1082@gmail.com>
2021-03-20 11:51:15 +0900

    initial commit

---
+++ src/main/java/com/remember/dream/DreamApplication.java
@@ -0,0 +1,13 @@
+package com.remember.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/remember/dream/DreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remember.dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
